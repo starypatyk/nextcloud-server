5bfb341d5d6db203de7d03700b87f0b45d6fa6cb
Dariusz Olszewski <starypatyk@users.noreply.github.com>
2017-12-29 22:04:42 +0100

    Proposed fix to improve performance of setupFS method

--- lib/private/legacy/util.php
+++ lib/private/legacy/util.php
@@ -290,6 +290,7 @@ class OC_Util {
 			OC_Hook::emit('OC_Filesystem', 'setup', array('user' => $user, 'user_dir' => $userDir));
 		}
 		\OC::$server->getEventLogger()->end('setup_fs');
+		self::$fsSetup = true;
 		return true;
 	}
 
